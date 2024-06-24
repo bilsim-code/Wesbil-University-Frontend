a4b14f1b3bc74012b9b331425552b94f0942dc7c
bilsim-code <bildadsimiyu6@gmail.com>
2024-06-24 20:21:33 +0300

    updated eregistrar

--- src/Pages/ERegistrar/ERegistrar.jsx
+++ src/Pages/ERegistrar/ERegistrar.jsx
@@ -2,7 +2,18 @@ import './ERegistrar.css'
 
 const ERegistrar = () => {
   return (
-    <div>ERegistrar</div>
+    <div className='registrar'>
+      <div className="registrar-form">
+        <form className='form'>
+            <h2>Login</h2>
+            <label htmlFor="username">Enter your username: </label>
+            <input type="text" name='name' placeholder='username' id='username'/>
+            <label htmlFor="password">Enter your password: </label>
+            <input type="password" name="password" id="password" placeholder='password' />
+            <button>Login</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
